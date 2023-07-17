6720d452252723480be5eac00f8544779b8b7b7f
mattix10 <mateusz.salamon10@gmail.com>
2023-07-17 21:12:59 +0200

    import Ngxs module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+import { AppState } from './shared/app.state';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgxsModule.forRoot([AppState]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
