19ece475b5f722c91ed77a25da5694ea2bffe29f
mattix10 <mateusz.salamon10@gmail.com>
2023-07-17 21:26:38 +0200

    add utils to draw numbers

---
+++ src/app/utils/utils.ts
@@ -0,0 +1,16 @@
+export const getRandomNumber = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const drawNumbers = (): [number, number] => {
+  const min = 1;
+  const max = 60;
+
+  let number1 = getRandomNumber(min, max);
+  let number2 = getRandomNumber(min, max);
+
+  while (number2 === number1) {
+    number2 = getRandomNumber(min, max);
+  }
+
+  return [number1, number2];
+};
