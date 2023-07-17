c299804e87c54703e3fffa0dee62c631e62ec969
mattix10 <mateusz.salamon10@gmail.com>
2023-07-17 22:58:56 +0200

    correct commands

--- src/app/shared/app-actions.ts
+++ src/app/shared/app-actions.ts
@@ -1,32 +1,40 @@
-import { Resource } from '../app.component';
+import { Resource } from 'src/models/Resource';
 
 export class CreatePlayers {
-  static readonly type = '[Player] Create Players';
+  static readonly type = '[Player] CreatePlayers';
   constructor(public playerNames: string[]) {}
 }
 
 export class ClearPeople {
-  static readonly type = '[Player] Clear People';
+  static readonly type = '[Player] ClearPeople';
   constructor(public payload: any) {}
 }
 
-export class GetResourceType {
-  static readonly type = '[App] Get resource type';
-}
-
 export class LoadPeople {
-  static readonly type = '[Player] Load people';
+  static readonly type = '[Player] LoadPeople';
 }
 
 export class LoadStarships {
-  static readonly type = '[Player] Load starships';
+  static readonly type = '[Player] LoadStarships';
+}
+
+export class GetResourceType {
+  static readonly type = '[App] GetResourceType';
 }
 
 export class ChangeResource {
-  static readonly type = '[App] Change resource';
+  static readonly type = '[App] ChangeResource';
   constructor(public resource: Resource) {}
 }
 
 export class DrawNumbers {
-  static readonly type = '[App] Draw numbers';
+  static readonly type = '[App] DrawNumbers';
+}
+
+export class ResetData {
+  static readonly type = '[App] ResetData';
+}
+
+export class CheckWinner {
+  static readonly type = '[App] CheckWinner';
 }
