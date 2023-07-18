8cd2da2a2942f478850ef2c6a7663087913b6c01
mattix10 <mateusz.salamon10@gmail.com>
2023-07-18 23:31:35 +0200

    add unit tests to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NgxsModule, Store } from '@ngxs/store';
+import { AppState } from './shared/app.state';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Resource } from 'src/models/Resource';
+import { CreatePlayers } from './shared/app-actions';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let store: Store;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [NgxsModule.forRoot([AppState]), HttpClientTestingModule],
     }).compileComponents();
+
+    store = TestBed.inject(Store);
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    // const fixture = TestBed.createComponent(AppComponent);
-    // const app = fixture.componentInstance;
-    // expect(app).toBeTruthy();
+    expect(app).toBeTruthy();
+  });
+
+  it('should create two players', async () => {
+    await store.dispatch(new CreatePlayers(['My Player 1', 'My Player 2']));
+
+    const { players } = store.selectSnapshot(AppState);
+    expect(players.length).toBe(2);
+  });
+
+  it("should change resource to 'Starship' after changing dropdown value", () => {
+    app.handleSelectedOption(Resource.Starships);
+    fixture.detectChanges();
+
+    const { resource } = store.selectSnapshot(AppState);
+    expect(resource).toEqual(Resource.Starships);
+  });
+
+  it("should draw 2 numbers after clicking 'Play' button", () => {
+    app.handlePlayButton();
+    fixture.detectChanges();
+
+    const { drawNumbers } = store.selectSnapshot(AppState);
+    expect(drawNumbers.length).toBe(2);
   });
 });
