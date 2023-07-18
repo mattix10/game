97602c7fcb985a8b5f5537e398c5e2af71fc0862
mattix10 <mateusz.salamon10@gmail.com>
2023-07-18 18:54:49 +0200

    add unit tests to game control component

--- src/app/shared/components/game-control/game-control.component.spec.ts
+++ src/app/shared/components/game-control/game-control.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameControlComponent } from './game-control.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Resource } from 'src/models/Resource';
 
 describe('GameControlComponent', () => {
   let component: GameControlComponent;
@@ -8,9 +12,12 @@ describe('GameControlComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameControlComponent ]
-    })
-    .compileComponents();
+      imports: [
+        GameControlComponent,
+        BrowserAnimationsModule,
+        MockDropdownComponent,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameControlComponent);
     component = fixture.componentInstance;
@@ -20,4 +27,37 @@ describe('GameControlComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit the event when the button is clicked', () => {
+    const outputSpy = spyOn(component.onPlayButtonClick, 'emit');
+
+    const buttonElement: HTMLButtonElement =
+      fixture.nativeElement.querySelector('.play-button');
+    buttonElement.click();
+
+    expect(outputSpy).toHaveBeenCalled();
+  });
+
+  it('should emit the event when a specific action is triggered on the child component', () => {
+    const childComponent: MockDropdownComponent = fixture.debugElement.query(
+      By.directive(MockDropdownComponent)
+    ).componentInstance;
+
+    const outputSpy = spyOn(component.onSelectedOptionChange, 'emit');
+    childComponent.onSelectedOptionChange();
+
+    expect(outputSpy).toHaveBeenCalled();
+  });
 });
+@Component({
+  selector: 'app-mock-dropdown',
+  template: '',
+  standalone: true,
+})
+class MockDropdownComponent {
+  @Output() selectedOptionChange = new EventEmitter<Resource>();
+
+  onSelectedOptionChange() {
+    this.selectedOptionChange.emit(Resource.People);
+  }
+}
