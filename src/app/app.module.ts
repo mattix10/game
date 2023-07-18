c6b86ae96153d23a1bf9841085c499bd474e1c13
mattix10 <mateusz.salamon10@gmail.com>
2023-07-18 14:51:55 +0200

    update template and app imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxsModule } from '@ngxs/store';
 import { AppState } from './shared/app.state';
-import { ResourceDropdownComponent } from './shared/components/resource-dropdown/resource-dropdown.component';
-import { MatButtonModule } from '@angular/material/button';
 import { PersonCardComponent } from './shared/components/person-card/person-card.component';
 import { StarshipCardComponent } from './shared/components/starship-card/starship-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { PlayerInfoComponent } from './shared/components/player-info/player-info.component';
+import { DrawnNumbersComponent } from './shared/components/drawn-numbers/drawn-numbers.component';
+import { GameControlComponent } from './shared/components/game-control/game-control.component';
 @NgModule({
-  declarations: [AppComponent, PersonCardComponent, StarshipCardComponent, PlayerInfoComponent],
+  declarations: [
+    AppComponent,
+    PersonCardComponent,
+    StarshipCardComponent,
+    PlayerInfoComponent,
+    DrawnNumbersComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ResourceDropdownComponent,
-    MatButtonModule,
+    GameControlComponent,
     MatCardModule,
     MatDividerModule,
     MatProgressSpinnerModule,
