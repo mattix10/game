be9b11a632b0911b27bc4e498bb59d72667acd16
mattix10 <mateusz.salamon10@gmail.com>
2023-07-18 16:20:49 +0200

    add info wrapper component

---
+++ src/app/shared/components/info-wrapper/info-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../app.state';
+
+@Component({
+  selector: 'app-info-wrapper',
+  templateUrl: './info-wrapper.component.html',
+  styleUrls: ['./info-wrapper.component.scss'],
+})
+export class InfoWrapperComponent {
+  @Select(AppState.loading) loading$!: Observable<boolean>;
+  @Select(AppState.error) error$!: Observable<string | null>;
+  @Input() drawnNumbers: number[] = [];
+}
