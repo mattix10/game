0b76766f27731164963770076f912233b59c3c9c
mattix10 <mateusz.salamon10@gmail.com>
2023-07-18 20:49:40 +0200

    add info wrapper unit tests

--- src/app/shared/components/info-wrapper/info-wrapper.component.spec.ts
+++ src/app/shared/components/info-wrapper/info-wrapper.component.spec.ts
@@ -1,17 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InfoWrapperComponent } from './info-wrapper.component';
+import { NgxsModule, Store } from '@ngxs/store';
+import { AppState } from '../../app.state';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AppStateModel } from 'src/models/AppStateModel';
+import { Resource } from 'src/models/Resource';
+import { By } from '@angular/platform-browser';
 
-describe('InfoWrapperComponent', () => {
+fdescribe('InfoWrapperComponent', () => {
   let component: InfoWrapperComponent;
   let fixture: ComponentFixture<InfoWrapperComponent>;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InfoWrapperComponent ]
-    })
-    .compileComponents();
+      declarations: [InfoWrapperComponent],
+      imports: [HttpClientTestingModule, NgxsModule.forRoot([AppState])],
+    }).compileComponents();
 
+    store = TestBed.inject(Store);
     fixture = TestBed.createComponent(InfoWrapperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +26,42 @@ describe('InfoWrapperComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display initial text', () => {
+    const parentDiv = fixture.debugElement.query(By.css('.info-wrapper'));
+    const childDivs = parentDiv.queryAll(By.css('div'));
+
+    expect(childDivs[0].nativeElement.textContent).toEqual(
+      " Select resource and click 'Play' button to start a game. "
+    );
+  });
+
+  it('should return error message', () => {
+    const mockStateData: AppStateModel = {
+      ...store.snapshot(),
+      error: 'Error message',
+    };
+
+    store.reset(mockStateData);
+    const error = store.selectSnapshot(({ error }) => error);
+    expect(error).toBe('Error message');
+  });
+
+  it('should return loading as true', () => {
+    const mockStateData: AppStateModel = {
+      ...store.snapshot(),
+      loading: true,
+    };
+
+    store.reset(mockStateData);
+    const loading = store.selectSnapshot(({ loading }) => loading);
+    expect(loading).toBe(true);
+  });
+
+  it('should have numbers 1 and 2 to as drawnNumbers', () => {
+    const drawnNumbers = [1, 2];
+    component.drawnNumbers = drawnNumbers;
+    expect(component.drawnNumbers).toEqual(drawnNumbers);
   });
 });
