3f6755b93357872f30b3344c437805c7e880972d
mattix10 <mateusz.salamon10@gmail.com>
2023-07-19 03:11:06 +0200

    add cypress commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,42 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+Cypress.Commands.add('getElementsAndCompareText', () => {
+  cy.get('mat-card-subtitle').as('elements');
+
+  cy.get('@elements').should('have.length', 2);
+
+  cy.get('@elements').invoke('text').as('textContents');
+
+  cy.get('@elements').then((els) => {
+    const [player1Text, player2Text] = Cypress.$.makeArray(els)
+      .map((el) => el.innerText)
+      .map((txt) => txt.split(' ')[1]);
+
+    return cy.wrap({
+      player1Text: parseInt(player1Text),
+      player2Text: parseInt(player2Text),
+    });
+  });
+});
+
+Cypress.Commands.add(
+  'verifyParagraphsContainWords',
+  (selector, attributesInParagrpahs) => {
+    cy.get(selector).as('parent');
+    cy.get('@parent')
+      .find('.card')
+      .each(($child) => {
+        cy.wrap($child)
+          .find('p')
+          .each(($paragraph) => {
+            const paragraphText = $paragraph.text();
+
+            const wordFound = attributesInParagrpahs.some((word) =>
+              paragraphText.includes(word)
+            );
+
+            expect(wordFound).to.be.true;
+          });
+      });
+  }
+);
